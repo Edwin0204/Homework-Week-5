965881fe2b08feb98d4663070b0f659b8de65e4f
Edwin0204 <edwins0204@gmail.com>
2022-04-10 11:51:14 -0500

    made the html container boxes

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+var timeDisplayEl = $('#time-display');
+
+
+// handle displaying the time
+function displayTime() {
+  var rightNow = moment().format('MMM DD, YYYY');
+  timeDisplayEl.text(rightNow);
+}
+
+
+
+setInterval(displayTime);
