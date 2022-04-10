6860b15f04414f1ebf8d0d0c25230d0ff13a4884
Edwin0204 <edwins0204@gmail.com>
2022-04-10 13:24:37 -0500

    made the function for the first 2 buttons

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,40 @@
 var timeDisplayEl = $('#time-display');
 
-
-// handle displaying the time
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY');
   timeDisplayEl.text(rightNow);
 }
 
+function hours(){
 
+  var time = new Date();
+  var currentHour = time.getHours();
 
+  for (var i = 8; i < 18; i++) {
+    if (i < currentHour) {
+      document.getElementById(i.toString()).classList.add('past');
+    } else if (i === currentHour) {
+      document.getElementById(i.toString()).classList.add('present');
+    } else if (i > currentHour) {
+      document.getElementById(i.toString()).classList.add('future');
+    }
+  }
+}
+
+jQuery(function(){
+  $("#btn8").click(function() {
+    var am8 = $('#8').val();
+    localStorage.setItem('8am', am8);
+  });
+  $("#btn9").click(function() {
+    var am9 = $('#9').val();
+    localStorage.setItem('9am', am9);
+  });
+  var act8 = localStorage.getItem('8am');
+  $('#8').val(act8);
+  var act9 = localStorage.getItem('9am');
+  $('#9').val(act9);
+});
+
+hours();
 setInterval(displayTime);
